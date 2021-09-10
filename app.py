1d2b4a7646679cfc1d6d06b925b5d6480d2e3506
Leak <sethpowellbusiness@gmail.com>
2021-09-10 14:57:56 -0700

    floundering a bit at the moment but getting closer to working

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
 import redis
-r = redis.Redis()
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from rq import Queue
+from rq.job import Job
+from worker import conn
 
-class Redis(object):
-    def __init__(self, host='localhost', port=6379,
-        db=0, password=None, socket_timeout=None)
+from models import *
+
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+q = Queue(connection=conn)
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+CORS(app)
+
+def urlShorten(r: redis.Redis, myurl: str) -> None:
+    with r.pipeline() as pipe:
+        error_count = 0
+        while True:
+            try:
+                pipe.watch(myurl)
+                storing = r.hset(myurl, )
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
